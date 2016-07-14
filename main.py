bdbef354302834f409cef6bdbbdb9d310409be20
Daniel Malmer <dmalmer@gmail.com>
2016-07-14 14:50:41 -0400

    Add basic output

--- main.py
+++ main.py
@@ -5,10 +5,15 @@ if __name__ == '__main__':
     with open('./venue_urls') as f:
         venue_urls = [line.strip() for line in f]
     
+    #TODO: create database for event entries
+    all_events = []
     for url in venue_urls:
+        print('Parsing', url)
         venue_parser = Parser.factory(url)
 
         soup = venue_parser.retrieve_soup()
-        events = venue_parser.parse_events(soup)
+        all_events.extend(venue_parser.parse_events(soup))
 
+    for event in sorted(all_events):
+        print(event)
 
